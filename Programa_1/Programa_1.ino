6d60e6c9d75307b241417f0e80726362484f117a
mherranzcruz2017 <36241957+mherranzcruz@users.noreply.github.com>
2018-04-23 17:05:51 +0200

    Progrma 1
    Ampliar programa per comandar 3 leds en comptes de 1

--- Programa_1/Programa_1.ino
+++ Programa_1/Programa_1.ino
@@ -10,17 +10,25 @@
 
 //***************************VARIABLES****************************
 const int led0 = 9;          // donar nom al pin 9 de l’Arduino
+const int led1 = 10;          // donar nom al pin 10 de l’Arduino
+const int led2 = 11;          // donar nom al pin 11 de l’Arduino
 const int ldr0 = A0;         // donar nom al pin A0 de l’Arduino
+const int ldr1 = A1;         // donar nom al pin A1 de l’Arduino
+const int ldr2 = A2;         // donar nom al pin A2 de l’Arduino
 
 int velocitat = 20;          // velocitat de l'acció en ms
 int valLdr0;                 // guardar valor del ldr
+int valLdr1;                 // guardar valor del ldr
+int valLdr2;                 // guardar valor del ldr
 
 //*****************************SETUP******************************
 
 void setup() {                 //Configura el final de salida
   
-   pinMode(led0, OUTPUT);     // definir el pin 9 com una sortida
-   
+   pinMode(led0, OUTPUT);     // definir el pin 9  com una sortida
+   pinMode(led1, OUTPUT);     // definir el pin 10 com una sortida
+   pinMode(led2, OUTPUT);     // definir el pin 11 com una sortida
+   Serial.begin(9600);
 }
 
 
@@ -30,13 +38,30 @@ void setup() {                 //Configura el final de salida
 //******************************LOOP******************************
 
 void loop() {                 //Inicia el bucle del programa
+ 
   valLdr0 = analogRead(ldr0);   // llegir valor ldr
   
-  valLdr0 = map(valLdr0, 0, 1023, 0, 255);   // mapejar valor de 0-1023 a 0-255
+  valLdr0 = map(valLdr0, 90,360 , 0, 255);   // mapejar valor de 0-1023 a 0-255
   
   analogWrite(led0, valLdr0);    // actualitzar l'estat del led
 
+  
 
+  valLdr1 = analogRead(ldr1);   // llegir valor ldr
+ 
+  
+  valLdr1 = map(valLdr1, 130, 520, 0, 255);   // mapejar valor de 0-1023 a 0-255
+  
+  analogWrite(led1, valLdr1);    // actualitzar l'estat del led
+
+  
+
+  valLdr2 = analogRead(ldr2);   // llegir valor ldr
+  Serial.println(valLdr2);
+  valLdr2 = map(valLdr2, 130, 400, 0, 255);   // mapejar valor de 0-1023 a 0-255
+  
+  analogWrite(led2, valLdr2);    // actualitzar l'estat del led
+ 
 }
 
 //****************************FUNCIONS****************************
